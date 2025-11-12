107a5de82d5ef074c03887b726b5a074a8de61fc
Mia25008 <mia.luzik@gmail.com>
2025-11-12 11:04:39 +0200

    Init

--- app/src/main/java/com/example/ftherapy/models/Item.java
+++ app/src/main/java/com/example/ftherapy/models/Item.java
@@ -2,7 +2,44 @@ package com.example.ftherapy.models;
 
 public class Item {
     public Integer price;
+
+    public Item(Integer price) {
+        this.price = price;
+    }
+
     public Integer itemName;
     public String description;
     public String imgPath;
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(Integer itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
 }
--- app/src/main/java/com/example/ftherapy/models/User.java
+++ app/src/main/java/com/example/ftherapy/models/User.java
@@ -7,4 +7,65 @@ public class User {
     public String password;
     public Boolean isAdmin;
     public Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public User(String id, String userName, String email, String password, Boolean isAdmin, Integer age) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = isAdmin;
+        this.age = age;
+    }
+
+    public User(String id) {
+        this.id = id;
+    }
 }
