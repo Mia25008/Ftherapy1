7964db512debe53ff5e021af63e824059ec9d54e
Mia25008 <mia.luzik@gmail.com>
2025-11-12 10:27:28 +0200

    Init

---
+++ app/src/main/java/com/example/ftherapy/models/Item.java
@@ -0,0 +1,8 @@
+package com.example.ftherapy.models;
+
+public class Item {
+    public Integer price;
+    public Integer itemName;
+    public String description;
+    public String imgPath;
+}
---
+++ app/src/main/java/com/example/ftherapy/models/User.java
@@ -0,0 +1,10 @@
+package com.example.ftherapy.models;
+
+public class User {
+    public String id;
+    public String userName;
+    public String email;
+    public String password;
+    public Boolean isAdmin;
+    public Integer age;
+}
